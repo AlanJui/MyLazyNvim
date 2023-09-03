fdc39c7ab655db4dac23db49bfc379ef94dd3fae
居正中 <AlanJui.1960@gmail.com>
2023-09-03 15:41:44 +0800

    Update

--- lua/plugins/LSP/init.lua
+++ lua/plugins/LSP/init.lua
@@ -15,12 +15,6 @@ return {
         },
       },
       { "williamboman/mason-lspconfig.nvim" },
-      {
-        "hrsh7th/cmp-nvim-lsp",
-        cond = function()
-          return require("lazyvim.util").has("nvim-cmp")
-        end,
-      },
       { "hrsh7th/cmp-nvim-lsp-signature-help" },
       {
         "smjonas/inc-rename.nvim",
